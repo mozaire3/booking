edac1d41a9b3f3968d8ca1e1bbe2382f9c5b3309
mozaire3 <mozaire.battah@epitech.eu>
2024-12-11 11:29:45 +0100

    [ADD]: api make

--- django_project/asgi.py
+++ django_project/asgi.py
@@ -1,7 +1,7 @@
 """
 ASGI config for django_project project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a modulevariable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
